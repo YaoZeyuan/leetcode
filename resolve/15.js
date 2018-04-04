3f518f41b2bf8b6cac2e0ef8a878438cc87023f3
姚泽源 <work@yaozeyuan.online>
2018-04-04 17:58:13 +0800

    feat : 更新15题的最优解

--- resolve/15.js
+++ resolve/15.js
@@ -1,77 +1,50 @@
 /**
- * 这个题非常坑, js的对象查找key的效率和Set比较非常低, 把Object换成Set后, 程序执行速度快了10倍 = =
+ * 这种解法的思路是, 利用排序数组中分别递增/递减最小最大值, 可以将所有可能出现的数都穷举遍的方法, 将O(N²)的计算量减成了O(N), 从而解决了问题
+ *
+ * 这个特征之前没听说过, 可以记一下
+ *
  * @param {number[]} nums
  * @return {number[][]}
  */
 var threeSum = function (nums) {
-  if (nums.length < 3) {
-    return []
-  }
+  //  从小到大排序
+  nums = nums.sort((a,b)=>{return a-b})
+  let res = new Set()
+  for (let base_item_index = 0; base_item_index < nums.length - 2; base_item_index++) {
+    if (base_item_index === 0 || (base_item_index > 0 && nums[base_item_index] !== nums[base_item_index - 1])) {
+      let smallest_item_index = base_item_index + 1
+      let largest_item_index = nums.length - 1
+      let sum = -nums[base_item_index]
+      while (smallest_item_index < largest_item_index) {
+        // 分别从最大最小值中取值, 相加后检查有没有可以和sum中和的值
+        if ((nums[smallest_item_index] + nums[largest_item_index]) === sum) {
+          // 有, 就将结果添加到Set里(自动去重)
+          res.add([nums[base_item_index], nums[smallest_item_index], nums[largest_item_index]].join('_'))
 
-  nums = nums.sort()
-  let number_map = {}
-  let number_set = new Set()
-  for (let num of nums) {
-
-    if (number_map[num]) {
-      number_map[num].push(num)
-    } else {
-      number_set.add(num)
-      number_map[num] = [num]
-    }
-  }
-  let result = {}
-  let has_target_0 = false
-  let is_length_less_3 = number_map[0] && number_map[0].length < 3
-  for (let first_index = 0; first_index < nums.length; first_index++) {
-    for (let second_index = first_index + 1; second_index < nums.length; second_index++) {
-      let target = -(nums[first_index] + nums[second_index])
-      if (nums[first_index] === nums[second_index] && 0 === target && number_set.has(target)) {
-        if (is_length_less_3) {
-          continue
-        }
-        if (has_target_0 === false) {
-          result['0_0_0'] = [0, 0, 0]
-        }
-        has_target_0 = true
-        continue
-      }
-      if (number_set.has(target)) {
-        let first_number = nums[first_index]
-        let second_number = nums[second_index]
-        let target = (second_number + first_number)
-
-        if (target === 0) {
-          if (number_map[0].length < 3) {
-            continue
+          // 跳过相同的值
+          while (smallest_item_index < largest_item_index && nums[smallest_item_index] === nums[smallest_item_index + 1]) {
+            smallest_item_index++
           }
+          while (smallest_item_index < largest_item_index && nums[largest_item_index] === nums[largest_item_index - 1]) {
+            largest_item_index--
+          }
+          // 最大值,最小值都往前移一位
+          smallest_item_index++
+          largest_item_index--
         } else {
-          if (-target === first_number || -target === second_number) {
-            if (number_map[-target].length < 2) {
-              continue
-            }
-          }
-        }
-        let raw_json
-        if (first_number < second_number) {
-          let a = first_number
-          first_number = second_number
-          second_number = a
-        }
-
-        if (-target > second_number) {
-          if (-target > first_number) {
-            raw_json = [-target, first_number, second_number]
+          // 没找到, 比较结果
+          if (nums[smallest_item_index] + nums[largest_item_index] < sum) {
+            // 不够 => 弃掉当前最小值, 取剩余元素中的最小值
+            smallest_item_index++
           } else {
-            raw_json = [first_number, -target, second_number]
+            // 过大 => 弃掉当前最大值, 取剩余元素中的最大值
+            largest_item_index--
           }
-        } else {
-          raw_json = [first_number, second_number, -target]
         }
-
-        result[raw_json.join('_')] = raw_json
       }
     }
   }
-  return Object.values(result)
+  let value_list = []
+  res.forEach((item) => {value_list.push(item.split('_').map(char=>{return parseInt(char)}))})
+  return value_list
 }
