87cbee66bde74f22527e6807ba9cc968960fb507
姚泽源 <work@yaozeyuan.online>
2018-04-04 14:17:06 +0800

    feat : 14题AC

--- resolve/14.js
+++ resolve/14.js
@@ -1,7 +1,7 @@
 function isPosSame (strs, pos) {
   let last_char = strs[0][pos]
   for(let str of strs){
-    if(last_char !== str[pos]){
+    if(last_char !== str[pos] || last_char === undefined){
       return false
     }
   }
@@ -15,6 +15,9 @@ function isPosSame (strs, pos) {
 var longestCommonPrefix = function(strs) {
   if(strs.length === 0){
     return ''
+  }
+  if(strs.length === 1){
+    return strs[0]
   }
   let result = []
 
