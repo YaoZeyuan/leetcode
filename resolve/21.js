a38fd7b328d29148852c570544e2092abca4f5f2
姚泽源 <work@yaozeyuan.online>
2018-04-06 18:21:24 +0800

    feat : 21题暴力AC

--- resolve/21.js
+++ resolve/21.js
@@ -1,13 +1,3 @@
-// function ListNode(val) {
-//     this.val = val;
-//     this.next = null;
-// }
-
-/**
- * 核心在于链表和数组的互换, 转换完成后就简单多了
- * @param header
- * @return {Array}
- */
 function list2Array (header) {
   let result = []
   while(header !== null){
@@ -32,7 +22,15 @@ function array2List (array) {
   }
   return header
 }
-
+var mergeKLists = function(lists) {
+  let transfer_list = []
+  for(let list of lists){
+    let array_list = list2Array(list)
+    transfer_list = transfer_list.concat(array_list)
+  }
+  transfer_list = transfer_list.sort((a,b)=>{return a-b})
+  return array2List(transfer_list)
+};
 
 
 /**
@@ -43,15 +41,10 @@ function array2List (array) {
  * }
  */
 /**
- * @param {ListNode[]} lists
+ * @param {ListNode} l1
+ * @param {ListNode} l2
  * @return {ListNode}
  */
-var mergeKLists = function(lists) {
-  let transfer_list = []
-  for(let list of lists){
-    let array_list = list2Array(list)
-    transfer_list = transfer_list.concat(array_list)
-  }
-  transfer_list = transfer_list.sort((a,b)=>{return a-b})
-  return array2List(transfer_list)
+var mergeTwoLists = function(l1, l2) {
+  return mergeKLists([l1,l2])
 };
