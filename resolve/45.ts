7f717b155fa778c17c659240a2d5c52f5f7f61b0
yaozeyuan <yaozeyan93@gmail.com>
2020-06-03 23:29:50 +0800

    feat: 初始化项目

---
+++ resolve/45.ts
@@ -0,0 +1,46 @@
+function jump(nums: number[]): number {
+  // 思路
+  // 1. 步数为0
+  // 1. 步数+1
+  // 1. 获取第一个值
+  // 2. 检查是否可以跳到终点
+  // 3. 如果可以, 返回当前步数
+  // 4. 如果不能到达终点, 寻找当前能触达区间中, 在触达点上, 能跳的最远的点
+  // 2. 以该点为起始, 重复上诉流程
+  let initStep = 0;
+  function checkNums(roadNumList: number[], currentStep = 0): number {
+    if (roadNumList.length <= 1) {
+      return 0;
+    }
+
+    currentStep = currentStep + 1;
+    let currentCanReach = roadNumList[0];
+    if (currentCanReach >= roadNumList.length - 1) {
+      return currentStep;
+    } else {
+      // 寻找能触及的更远的点
+      let maxIndexAt = 0;
+      let maxJumpTo = 0;
+      for (let i = 1; i <= currentCanReach; i++) {
+        let thisCanReachAt = roadNumList[i] + i;
+        if (thisCanReachAt > maxJumpTo) {
+          maxJumpTo = thisCanReachAt;
+          maxIndexAt = i;
+        }
+      }
+      // maxIndexAt就是能跳的最远的
+      let nextRoadList = roadNumList.splice(maxIndexAt);
+      return checkNums(nextRoadList, currentStep);
+    }
+  }
+
+  return checkNums(nums, 0);
+}
+
+function test() {
+  let inputList = [2, 3, 1, 1, 4];
+  let step = jump(inputList);
+  console.log("need jump =>", step);
+}
+
+test();
