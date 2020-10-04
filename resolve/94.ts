9e15db47042d111cf24abfc7efd590ebe0c5dbce
yaozeyuan001 <yaozeyuan001@ke.com>
2020-10-04 15:43:27 +0800

    feat: 94题完成

---
+++ resolve/94.ts
@@ -0,0 +1,38 @@
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function inorderTraversal(root: TreeNode | null): number[] {
+  let result: number[] = [];
+
+  function traversal(node: TreeNode | null) {
+    if (node === null) {
+      return;
+    }
+    traversal(node.left);
+    result.push(node.val);
+    traversal(node.right);
+  }
+  traversal(root);
+  return result;
+}
