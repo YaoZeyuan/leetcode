846d30b738396378e2a58b0e391b585290133e70
yaozeyuan001 <yaozeyuan001@ke.com>
2020-10-08 21:50:09 +0800

    feat: e后边可以出现加号

--- resolve/65.ts
+++ resolve/65.ts
@@ -4,8 +4,9 @@ function isNumber(s: string): boolean {
   inputStr = inputStr.trim();
   // 先确保基本形式正确
   // 以下字符只能出现一次
-  // -号可以出现两次, -3e-5
-  let onlyOnceCharList = ["e", "+", "."];
+  // -/+号可以出现两次, -3e-5
+  // -/+号可以出现两次, +3e+5
+  let onlyOnceCharList = ["e", "."];
   for (let checkChar of onlyOnceCharList) {
     if (inputStr.split(checkChar).length > 2) {
       return false;
@@ -16,13 +17,14 @@ function isNumber(s: string): boolean {
     return false;
   }
   // +号只能出现在开头
-  if (inputStr.replace(/^\+/, "").includes("+")) {
+  if (inputStr.replace(/^\+/, "").replace(/e\+/, "").includes("+")) {
     return false;
   }
   // 剔除e/+/-/.之后, 只应剩下数字
   if (
     inputStr
       .replace(/^\+/, "")
+      .replace(/e\+/, "")
       .replace(/^-/, "")
       .replace(/e-/, "")
       .replace(/e/, "")
@@ -35,9 +37,9 @@ function isNumber(s: string): boolean {
   // 可以开始正常处理了
 
   // 带e
-  let case1 = /^(\+|-)?(\d+)?\.?\d+(e-?\d+)$/.test(inputStr);
+  let case1 = /^(\+|-)?(\d+)?\.?\d+(e(-|\+)?\d+)$/.test(inputStr);
   // 带e且小数点后没数字
-  let case2 = /^(\+|-)?(\d+)\.?(e-?\d+)$/.test(inputStr);
+  let case2 = /^(\+|-)?(\d+)\.?(e(-|\+)?\d+)$/.test(inputStr);
   // 不带e
   let case3 = /^(\+|-)?(\d+)?\.?\d+$/.test(inputStr);
   // 不带ee且小数点后没数字
@@ -51,6 +53,7 @@ function isNumber(s: string): boolean {
 let result65 = isNumber(
   // "123"
   //   "+1.1e23"
-  ".0 "
+  //   ".0 "
+  " 005047e+6"
 );
 console.log("result =>", result65);
