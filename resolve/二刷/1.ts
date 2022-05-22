2dd31112a0b28428db2f27ab6ab94e8655e2cec0
yaozeyuan001 <yaozeyuan001@ke.com>
2022-05-22 08:32:17 +0800

    feat: 填写解题思路

---
+++ resolve/二刷/1.ts
@@ -0,0 +1,12 @@
+function twoSum(nums: number[], target: number): number[] {
+  // 解题思路
+  // 1. 利用map, 建立num => index的数据库db
+  
+  
+  // 2. 循环检查nums数组, 从第一个数num开始, 检查db中是否有 target - num
+  // 2.1若存在, 返回num的下标和target-num的下标, 问题结束
+  // 2.2若不存在, 检查下一个数字.
+  // 3. 若完全不存在, 返回空数组
+
+
+}
