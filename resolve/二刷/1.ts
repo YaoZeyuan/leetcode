6709d5c4ab52705755a6e84a899cf3823d549427
yaozeyuan001 <yaozeyuan001@ke.com>
2022-05-22 08:40:17 +0800

    feat: 完成第一题, 同时完善项目配置, 以后刷题不需要watch

--- resolve/二刷/1.ts
+++ resolve/二刷/1.ts
@@ -1,12 +1,21 @@
 function twoSum(nums: number[], target: number): number[] {
   // 解题思路
   // 1. 利用map, 建立num => index的数据库db
-  
-  
+  let db = new Map<number, number>();
+  for (let i = 0; i < nums.length; i++) {
+    db.set(nums[i], i);
+  }
   // 2. 循环检查nums数组, 从第一个数num开始, 检查db中是否有 target - num
   // 2.1若存在, 返回num的下标和target-num的下标, 问题结束
   // 2.2若不存在, 检查下一个数字.
   // 3. 若完全不存在, 返回空数组
-
-
+  for (let i = 0; i < nums.length; i++) {
+    let checkNum = target - nums[i];
+    if (db.has(checkNum)) {
+      return [i, db.get(checkNum)];
+    }
+  }
+  return [];
 }
+
+console.log(twoSum([1, 2, 3, 4, 5], 6));
