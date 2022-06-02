cdb7b0615a3c98aae4aedf4ae35380a4aabda4df
yaozeyuan001 <yaozeyuan001@ke.com>
2022-06-02 08:57:31 +0800

    feat: 第108题完成

---
+++ resolve/二刷/108.ts
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function sortedArrayToBST(nums: number[]): TreeNode | null {
+    // 创建思路为
+    // 将数组分为 [左数组] 中间值 [右数组] 三部分, 只要左右之分小于2, 即可保证创建的子树高度相差不大于1
+    switch (nums.length) {
+        case 0:
+            return null
+        case 1:
+            return new TreeNode(nums[0], null, null)
+        case 2:
+            return new TreeNode(nums[1], new TreeNode(nums[0]), null)
+        case 3:
+            return new TreeNode(nums[1], new TreeNode(nums[0]), new TreeNode(nums[2]))
+    }
+
+    let midPos = Math.floor(nums.length / 2)
+
+    let leftList = nums.slice(0, midPos)
+    let rightList = nums.slice(midPos + 1)
+
+    return new TreeNode(nums[midPos], sortedArrayToBST(leftList), sortedArrayToBST(rightList))
+}
