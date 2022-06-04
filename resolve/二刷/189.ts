632fbeb4232d8ce9cd86e00ef9e3840d047d479a
yaozeyuan001 <yaozeyuan001@ke.com>
2022-06-05 00:59:09 +0800

    feat: 第189题完成

---
+++ resolve/二刷/189.ts
@@ -0,0 +1,15 @@
+/**
+ Do not return anything, modify nums in-place instead.
+ */
+function rotate(nums: number[], k: number): void {
+    let realRotate = k % nums.length
+    let counter = 0
+    while (counter < realRotate) {
+        let val = nums.pop()
+        nums.unshift(val)
+        counter++
+    }
+    return
+}
+
+console.log(rotate([0, 1, 2, 3, 4, 5], 20))
